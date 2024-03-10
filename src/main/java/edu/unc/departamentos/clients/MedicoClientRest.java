7852bf9d9a57cd1805adb9cfe1ec67d7fb1b7b3b
Ever Henry Abanto Vilchez <eabantov20_1@unc.edu.pe>
2024-03-10 13:21:13 -0500

    Conexion con el otro microservicio

---
+++ src/main/java/edu/unc/departamentos/clients/MedicoClientRest.java
@@ -0,0 +1,16 @@
+package edu.unc.departamentos.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="microServicesClinicas", url = "localhost:8080/api/medicos")
+public class MedicoClientRest {
+
+	 @DeleteMapping("/eliminar-medico-departamento/{id}")
+	 void eliminarMedicoDepaPorId(@PathVariable Long id) {
+		 
+	 }
+	
+	
+}
